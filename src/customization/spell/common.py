c7988c7e918594baac587ea5e8dcfa14a165859a
heshanu3d <33936333+heshanu3d@users.noreply.github.com>
2025-11-14 23:08:03 +0800

    增加spell sql方法, 增加shaman增强天赋 mod

--- src/customization/spell/common.py
+++ src/customization/spell/common.py
@@ -1,3 +1,11 @@
+def cond_conv2server(cond):
+    condition = cond
+    condition = condition.replace('s.id', 's.entry')
+    condition = condition.replace('s.spellname4', 'l.name_loc4')
+    condition = condition.replace('s.spellrank4', 'l.nameSubtext_loc4')
+    condition = condition.replace('s.SpellDescription4', 'l.description_loc4')
+    return condition
+
 def sql_update_dmg(table, multi, cond, effect_type):
     sql = f'''
         update {table} s set s.EffectBasePoints1=(s.EffectBasePoints1+1)*{multi}-1
@@ -31,6 +39,26 @@ def sql_update_trigger_chance(table, multi, cond, effect_type):
     '''
     return sql
 
+def sql_update_trigger_time(table, trigger_time, cond, effect_type):
+    sql = f'''
+        update {table} s set s.ProcCharges={trigger_time}
+        WHERE
+        s.procchance!=-1 and s.procchance!=0 and 
+        (
+            (s.Effect1={effect_type} and s.effectapplyauraname1=42 and s.EffectTriggerSpell1>0) or
+            (s.Effect2={effect_type} and s.effectapplyauraname2=42 and s.EffectTriggerSpell2>0) or
+            (s.Effect3={effect_type} and s.effectapplyauraname3=42 and s.EffectTriggerSpell3>0)
+        ) and ({cond});
+    '''
+    return sql
+
+def sql_update_duration(table, idx, cond, effect_type):
+    sql = f'''
+        update {table} s set s.DurationIndex={idx}
+        WHERE ({cond});
+    '''
+    return sql
+
 def sql_query(table, cond):
     return f'''
         select DISTINCT s.entry id
@@ -38,14 +66,6 @@ def sql_query(table, cond):
         INNER JOIN locales_spell l ON s.entry = l.entry
         WHERE {cond};
     '''
-
-def cond_conv2server(cond):
-    condition = cond
-    condition = condition.replace('s.id', 's.entry')
-    condition = condition.replace('s.spellname4', 'l.name_loc4')
-    condition = condition.replace('s.spellrank4', 'l.nameSubtext_loc4')
-    condition = condition.replace('s.SpellDescription4', 'l.description_loc4')
-    return condition
 
 class Mod:
     def __init__(self, instance, cond):
@@ -62,6 +82,10 @@ class Mod:
             sql_update = sql_update_dot_interval
         elif sql_type == 'trigger_chance':
             sql_update = sql_update_trigger_chance
+        elif sql_type == 'duration':
+            sql_update = sql_update_duration
+        elif sql_type == 'trigger_time':
+            sql_update = sql_update_trigger_time
         else:
             raise Exception('sql_type error')
 
@@ -99,6 +123,87 @@ class Mod:
             if spellname in cond.keys():
                 self.multi_effect_on_spell('spell',          multi_rate, cond[spellname], 6, sql_type='trigger_chance')
                 self.multi_effect_on_spell('spell_template', multi_rate, cond[spellname], 6, sql_type='trigger_chance')
+
+    def mod_duration(self, spellnames):
+            cond = self._cond
+            duration_dict = {
+                '100ms' : 407,
+                '250ms' : 328,
+                '500ms' : 327,
+                   '1s' :  36,
+                 '1.5s' :  65,
+                   '2s' :  39,
+                 '2.5s' :  66,
+                   '3s' :  27,
+                   '4s' :  35,
+                   '5s' :   7,
+                   '5s' :  28,
+                   '6s' :  32,
+                   '7s' : 165,
+                   '8s' :  31,
+                   '9s' : 105,
+                  '10s' :   1,
+                  '11s' :  38,
+                  '12s' :  29,
+                  '14s' : 305,
+                  '15s' :   8,
+                  '16s' : 387,
+                  '18s' :  85,
+                  '20s' :  18,
+                  '21s' :  86,
+                  '22s' : 467,
+                  '24s' : 106,
+                  '25s' :  63,
+                  '26s' : 468,
+                  '27s' : 205,
+                  '30s' :   9,
+                  '30s' :   2,
+                  '35s' : 125,
+                  '36s' : 325,
+                  '40s' :  64,
+                  '44s' : 326,
+                  '45s' :  22,
+                  '50s' : 245,
+                  '55s' : 265,
+                  '60s' :  10,
+                  '75s' :  62,
+                  '90s' :  23,
+                 '120s' :   4,
+                 '160s' :  24,
+                 '180s' :  25,
+                 '210s' : 552,
+                 '230s' :  16,
+                 '240s' :  26,
+                 '300s' :   5,
+                 '360s' :  41,
+                 '600s' :   6,
+                 '900s' : 347,
+                '1200s' :  40,
+                '1800s' :  30,
+                '2700s' : 145,
+                '3600s' :  42,
+                '5400s' : 547,
+                '7200s' : 367,
+               '10800s' : 548,
+               '14400s' : 527,
+               'infinit': 21,
+            }
+            for spellname, duration in spellnames.items():
+                if spellname in cond.keys():
+                    if duration in duration_dict:
+                        duration_idx = duration_dict[duration]
+                        self.multi_effect_on_spell('spell',          duration_idx, cond[spellname], 6, sql_type='duration')
+                        self.multi_effect_on_spell('spell_template', duration_idx, cond[spellname], 6, sql_type='duration')
+                    else:
+                        print(f'WARNING: {spellname} duration {duration} not in duration_dict')
+
+    def mod_trigger_time(self, spellnames):
+        cond = self._cond
+        for spellname, trigger_time in spellnames.items():
+            if spellname in cond.keys():
+                self.multi_effect_on_spell('spell',          trigger_time, cond[spellname], 6, sql_type='trigger_time')
+                self.multi_effect_on_spell('spell_template', trigger_time, cond[spellname], 6, sql_type='trigger_time')
+
 class Test:
     def __init__(self, helper, mod):
         self._helper = helper
@@ -111,15 +216,21 @@ class Test:
         self._helper.search(spell_and_multiratios.keys())
         self._mod.mod_dmg(spell_and_multiratios)
         self._helper.search(spell_and_multiratios.keys())
-
     def mod_dot_interval(self, spell_and_multiratios):
         self._helper.search(spell_and_multiratios.keys())
         self._mod.mod_dot_interval(spell_and_multiratios)
         self._helper.search(spell_and_multiratios.keys())
-    
     def mod_trigger_chance(self, spell_and_multiratios):
         self._helper.search(spell_and_multiratios.keys())
         self._mod.mod_trigger_chance(spell_and_multiratios)
+        self._helper.search(spell_and_multiratios.keys())
+    def mod_duration(self, spell_and_multiratios):
+        self._helper.search(spell_and_multiratios.keys())
+        self._mod.mod_duration(spell_and_multiratios)
+        self._helper.search(spell_and_multiratios.keys())
+    def mod_trigger_time(self, spell_and_multiratios):
+        self._helper.search(spell_and_multiratios.keys())
+        self._mod.mod_trigger_time(spell_and_multiratios)
         self._helper.search(spell_and_multiratios.keys())
 
 class Helper:
@@ -132,10 +243,11 @@ class Helper:
         sql = f'''
             select s.id,s.SpellName4 as name,s.SpellRank4 as lvl,s.Effect1 as e1,s.Effect2 as e2,s.Effect3 as e3,s.EffectBasePoints1 as base1,s.EffectBasePoints2 as base2,s.EffectBasePoints3 as base3
             ,s.effectapplyauraname1 as aura1,s.effectapplyauraname2 as aura2,s.effectapplyauraname3 as aura3
-            ,EffectAmplitude1 as amp1,EffectAmplitude2 as amp2,EffectAmplitude3 as amp3
-            ,EffectTriggerSpell1 trig1,procchance trigchance,procflags,EffectMiscValue1 as misc1
+            ,EffectAmplitude1 as amp1,EffectAmplitude2 as amp2
+            ,DurationIndex dur_idx
+            ,EffectTriggerSpell1 trig1,procchance trig_c,procflags proc_f,ProcCharges trig_time,EffectMiscValue1 as misc1
             ,CastingTimeIndex cast_idx,StartRecoveryCategory as r_class,StartRecoveryTime as r_time
-            # ,spelldescription4
+            # ,SpellDescription4 d
             from spell s
             WHERE {condition};
         '''
@@ -146,9 +258,11 @@ class Helper:
         sql = f'''
             select DISTINCT s.entry id,l.name_loc4 name,l.nameSubtext_loc4 as lvl,s.Effect1 as e1,s.Effect2 as e2,s.Effect3 as e3,s.EffectBasePoints1 as base1,s.EffectBasePoints2 as base2,s.EffectBasePoints3 as base3
             ,s.effectapplyauraname1 as aura1,s.effectapplyauraname2 as aura2,s.effectapplyauraname3 as aura3
-            ,EffectAmplitude1 as amp1,EffectAmplitude2 as amp2,EffectAmplitude3 as amp3
-            ,EffectTriggerSpell1 trig1,procchance trigchance,procflags,EffectMiscValue1 as misc1
+            ,EffectAmplitude1 as amp1,EffectAmplitude2 as amp2
+            ,DurationIndex dur_idx
+            ,EffectTriggerSpell1 trig1,procchance trig_c,procflags proc_f,ProcCharges trig_time,EffectMiscValue1 as misc1
             ,CastingTimeIndex cast_idx,StartRecoveryCategory as r_class,StartRecoveryTime as r_time
+            # ,description_loc4 d
             from spell_template s
             INNER JOIN locales_spell l ON s.entry = l.entry
             WHERE {condition};
--- src/customization/spell/shaman.py
+++ src/customization/spell/shaman.py
@@ -16,21 +16,34 @@ cond = {
     # '腐蚀术' : "s.spellname4='腐蚀术'    and s.spellrank4 like '等级%' and startrecoverytime>0",
     # '痛苦诅咒' : "s.spellname4='痛苦诅咒'    and s.spellrank4 like '等级%' and startrecoverytime>0",
 
-    # '闪电箭' : "s.spellname4='闪电箭'    and s.spellrank4 like '等级%' and startrecoverytime>0 and ((s.id<10393 or s.id=15207 or s.id=15208) and s.id!=8246)",
-    # '闪电链' : "s.spellname4='闪电链'    and s.spellrank4 like '等级%' and startrecoverytime>0",
-    # '闪电之盾' : "s.spellname4='闪电之盾'    and s.spellrank4 like '等级%' and startrecoverytime>0",
-    # '地震术' : "s.spellname4='地震术'    and s.spellrank4 like '等级%' and startrecoverytime>0",
-    # '烈焰震击' : "s.spellname4='烈焰震击' and s.spellrank4 like '等级%' and startrecoverytime>0",
-    # '冰霜震击' : "s.spellname4='冰霜震击' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '闪电箭' : "s.spellname4='闪电箭'    and s.spellrank4 like '等级%' and startrecoverytime>0 and ((s.id<10393 or s.id=15207 or s.id=15208) and s.id!=8246)",
+    '闪电链' : "s.spellname4='闪电链'    and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '闪电之盾' : "s.spellname4='闪电之盾'    and s.spellrank4 like '等级%' and startrecoverytime>0 and EffectTriggerSpell1>0 and procflags=139944",
+    '地震术' : "s.spellname4='地震术'    and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '烈焰震击' : "s.spellname4='烈焰震击' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '冰霜震击' : "s.spellname4='冰霜震击' and s.spellrank4 like '等级%' and startrecoverytime>0",
 
-    # '石化武器' : "s.spellname4='石化武器' and s.spellrank4 like '等级%' and startrecoverytime>0",
-    # '火舌武器' : "s.spellname4='火舌武器' and s.spellrank4 like '等级%' and startrecoverytime>0",
-    # '冰封武器' : "s.spellname4='冰封武器' and s.spellrank4 like '等级%' and startrecoverytime>0",
-    # '风怒武器' : "s.spellname4='风怒武器' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    # TODO 1
+    '石化武器' : "s.spellname4='石化武器' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '火舌武器' : "s.spellname4='火舌武器' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '冰封武器' : "s.spellname4='冰封武器' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '风怒武器' : "s.spellname4='风怒武器' and s.spellrank4 like '等级%' and startrecoverytime>0",
 
-    # '火焰新星图腾' : "s.spellname4='火焰新星图腾' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    # TODO 2
+    '火焰新星图腾' : "s.spellname4='火焰新星图腾' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '灼热图腾' : "s.spellname4='灼热图腾' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '地缚图腾' : "s.spellname4='地缚图腾' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '石爪图腾' : "s.spellname4='石爪图腾' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '大地之力图腾' : "s.spellname4='大地之力图腾' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '清毒图腾' : "s.spellname4='大地之力图腾' and s.spellrank4 like '等级%' and startrecoverytime>0",
 
-    # '风暴打击' : "s.spellname4='风暴打击' and s.spellrank4 like '等级%' and startrecoverytime>0",
+    # TODO 3
+    '次级治疗波' : "s.spellname4='次级治疗波'    and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '治疗波' : "s.spellname4='治疗波'    and s.spellrank4 like '等级%' and startrecoverytime>0",
+    '消毒术' : "s.spellname4='消毒术'    and startrecoverytime>0",
+    '祛病术' : "s.spellname4='祛病术'    and startrecoverytime>0",
+
+    # '幽魂之狼' : "s.spellname4='幽魂之狼' and s.spellrank4 like '等级%'",
 
     # '剑专精' : "s.spellname4='剑类武器专精' and s.spellrank4 like '等级%' and effectapplyauraname1=42 and effectbasepoints1!=-1",
     # 'test' : "s.id=16459",
@@ -38,31 +51,38 @@ cond = {
     
 
     # 元素天赋
-    # '震荡'    : "s.spellname4='震荡'            and s.spellrank4 like '等级%'",
-    # '传导'    : "s.spellname4='传导'            and s.spellrank4 like '等级%'",
-    # '大地之握' : "s.spellname4='大地之握'        and s.spellrank4 like '等级%'",
-    # '元素防护' : "s.spellname4='元素防护'        and s.spellrank4 like '等级%'",
-    # '烈焰召唤' : "s.spellname4='烈焰召唤'        and s.spellrank4 like '等级%'",
-    # '元素集中' : "s.spellname4='元素集中'        and s.EffectTriggerSpell1>0",
-    # '回响'    : "s.spellname4='回响'            and s.spellrank4 like '等级%'",
-    # '雷霆召唤' : "s.spellname4='雷霆召唤'        and s.spellrank4 like '等级%'",
-    # '强化火焰图腾' : "s.spellname4='强化火焰图腾' and s.spellrank4 like '等级%'",
-    # '风暴之眼' : "s.spellname4='风暴之眼'        and s.spellrank4 like '等级%'",
-    # '元素浩劫' : "s.spellname4='元素浩劫'        and s.spellrank4 like '等级%' and effectapplyauraname1=52",
-    # '风暴来临' : "s.spellname4='风暴来临'        and s.spellrank4 like '等级%'",
-    # '元素之怒' : "s.spellname4='元素之怒'        and s.effectapplyauraname1=108",
-    # '闪电掌握' : "s.spellname4='闪电掌握'        and s.spellrank4 like '等级%'",
-    # '元素掌握' : "s.spellname4='元素掌握'        and s.effectbasepoints2!=0",
+    '震荡'    : "s.spellname4='震荡'            and s.spellrank4 like '等级%'",
+    '传导'    : "s.spellname4='传导'            and s.spellrank4 like '等级%'",
+    '大地之握' : "s.spellname4='大地之握'        and s.spellrank4 like '等级%'",
+    '元素防护' : "s.spellname4='元素防护'        and s.spellrank4 like '等级%'",
+    '烈焰召唤' : "s.spellname4='烈焰召唤'        and s.spellrank4 like '等级%'",
+    '元素集中' : "s.spellname4='元素集中'        and s.EffectTriggerSpell1>0",
+    '回响'    : "s.spellname4='回响'            and s.spellrank4 like '等级%'",
+    '雷霆召唤' : "s.spellname4='雷霆召唤'        and s.spellrank4 like '等级%'",
+    '强化火焰图腾' : "s.spellname4='强化火焰图腾' and s.spellrank4 like '等级%'",
+    '风暴之眼' : "s.spellname4='风暴之眼'        and s.spellrank4 like '等级%'",
+    '元素浩劫' : "s.spellname4='元素浩劫'        and s.spellrank4 like '等级%' and effectapplyauraname1=52",
+    '风暴来临' : "s.spellname4='风暴来临'        and s.spellrank4 like '等级%'",
+    '元素之怒' : "s.spellname4='元素之怒'        and s.effectapplyauraname1=108",
+    '闪电掌握' : "s.spellname4='闪电掌握'        and s.spellrank4 like '等级%'",
+    '元素掌握' : "s.spellname4='元素掌握'        and s.effectbasepoints2!=0",
 
     # 增强天赋
-    # '先祖知识' : "s.spellname4='先祖知识'        and s.spellrank4 like '等级%'",
-    # '盾牌专精' : "s.spellname4='盾牌专精'        and s.spellrank4 like '等级%' and effectapplyauraname2=150",
-    # '守护图腾' : "s.spellname4='守护图腾'        and s.spellrank4 like '等级%'",
-    # '雷鸣猛击' : "s.spellname4='雷鸣猛击'        and s.spellrank4 like '等级%'",
+    '先祖知识' : "s.spellname4='先祖知识'        and s.spellrank4 like '等级%'",
+    '盾牌专精' : "s.spellname4='盾牌专精'        and s.spellrank4 like '等级%' and effectapplyauraname2=150",
+    '守护图腾' : "s.spellname4='守护图腾'        and s.spellrank4 like '等级%'",
+    '雷鸣猛击' : "s.spellname4='雷鸣猛击'        and s.spellrank4 like '等级%'",
     '强化幽魂之狼' : "s.spellname4='强化幽魂之狼' and s.spellrank4 like '等级%'",
-    '幽魂之狼' : "s.spellname4='幽魂之狼' and s.spellrank4 like '等级%'",
-    # '强化闪电之盾' : "s.spellname4='强化闪电之盾' and s.spellrank4 like '等级%'",
-    
+    '强化闪电之盾' : "s.spellname4='强化闪电之盾' and s.spellrank4 like '等级%'",
+    '强化图腾' : "s.spellname4='强化图腾' and s.spellrank4 like '等级%' and effectapplyauraname2!=0",
+    '双手斧和锤' : "s.spellname4='双手斧和锤'",
+    '预知' : "s.spellname4='预知' and s.spellrank4 like '等级%' and effectapplyauraname1=49",
+    '乱舞' : "s.spellname4='乱舞' and s.spellrank4 like '等级%' and effectapplyauraname1=138 and (s.id=16257 or s.id=16277 or s.id=16278 or s.id=16279 or s.id=16280) and procflags=4",
+    '坚韧' : "s.spellname4='坚韧' and s.spellrank4 like '等级%' and (s.id=16252 or s.id=16306 or s.id=16307 or s.id=16308 or s.id=16309) and EffectMiscValue1=1",
+    '强化武器图腾' : "s.spellname4='强化武器图腾' and s.spellrank4 like '等级%'",
+    '元素武器' : "s.spellname4='元素武器' and s.spellrank4 like '等级%' and Effect2!=0",
+    '武器掌握' : "s.spellname4='武器掌握' and s.spellrank4 like '等级%'",
+    '风暴打击' : "s.spellname4='风暴打击' and s.spellrank4 like '等级%' and startrecoverytime>0",
 }
 
 mod_dmg_skills = {
@@ -77,7 +97,7 @@ mod_talent_skills = {
     '烈焰震击' : 2,
     '闪电之盾' : 2,
 
-    '震荡'    : 10,
+    # '震荡'    : 10,
     '传导'    : 5,
     '大地之握' : 3,
     '元素防护' : 3,
@@ -96,9 +116,16 @@ mod_talent_skills = {
     '盾牌专精' : 4,
     '守护图腾' : 5,
     '雷鸣猛击' : 2,
-    '强化幽魂之狼' : 2,
-    '强化闪电之盾' : 2,
-
+    '强化幽魂之狼' : 1.5,
+    '强化闪电之盾' : 10,
+    '强化图腾' : 10,
+    '预知' : 4,
+    '乱舞' : 4,
+    '坚韧' : 4,
+    '强化武器图腾' : 10,
+    '元素武器' : 10,
+    '武器掌握' : 4,
+    '风暴打击': 3
 }
 
 mod_dot_interval_skills = {
@@ -107,6 +134,14 @@ mod_dot_interval_skills = {
 
 mod_trigger_chance_skills = {
     '元素集中' : 2,
+}
+
+mod_duration_skills = {
+    '闪电之盾' : '1800s',
+}
+
+mod_trigger_time_skills = {
+    '闪电之盾' : 60,
 }
 
 gcd_eq0_skills = [
@@ -146,6 +181,26 @@ def mod_gcd(instance, gcd_ms, spellnames):
     #     print(sql)
     instance.execute_multi_sqls(sqls)
 
+# 双手斧和锤
+# 196:单手斧，197:双手斧，198:单手锤，199:双手锤，200:长柄武器，201:单手剑，202:双手剑，227:法杖
+def mod_spell_16269(instance):
+    # 使你可以使用双手斧和双手锤 -> 使你可以使用双手斧,双手锤和双手剑
+    spellname = '双手斧和锤'
+    description = '使你可以使用双手斧,双手锤和双手剑'
+    if spellname in cond:
+        results = instance.execute_sql_with_retval(sql_query('spell_template', cond_conv2server(cond[spellname])))
+        entry_condition_str = ' or '.join([f"s.entry={item[0]}" for item in results])
+        sqls = f'''
+                update spell s set Effect3=36,EffectImplicitTargetA3=1,EffectTriggerSpell3=202,SpellDescription4='{description}'
+                where ({cond[spellname]});
+                update spell_template s set Effect3=36,EffectImplicitTargetA3=1,EffectTriggerSpell3=202
+                where ({entry_condition_str});
+                update locales_spell s set description_loc4='{description}'
+                where ({entry_condition_str});
+        '''
+        # print(sqls)
+        instance.execute_multi_sqls(sqls)
+
 def customize(instance):
     print(f'{__name__:<45}start to costomize!')
 
@@ -153,16 +208,20 @@ def customize(instance):
     mod = Mod(instance, cond)
     test = Test(helper, mod)
 
+    # helper.search(['闪电之盾'])
+
     # test.mod_dmg({'烈焰震击':1.5})
     # test.mod_talent({'震荡':10})
     # test.mod_dot_interval({'烈焰震击':0.8})
     # test.mod_dot_interval({'烈焰震击':1.25})
     # test.mod_trigger_chance({'元素集中':2})
+    # test.mod_duration({'闪电之盾' : '1800s'})
+    # test.mod_trigger_time({'闪电之盾' : 60})
 
     # helper.search(all_spellnames, 'spell')
     # helper.search(all_spellnames, 'spell_template')
     # 等价于以上两句
-    helper.search(all_spellnames)
+    # helper.search(all_spellnames)
 
     # 调整 部分萨满技能伤害至 x倍
     # mod.mod_dmg(mod_dmg_skills)
@@ -172,6 +231,14 @@ def customize(instance):
     # mod.mod_dot_interval(mod_dot_interval_skills)
     # 调整 部分萨满技能触发几率至 x倍
     # mod.mod_trigger_chance(mod_trigger_chance_skills)
+    # 调整 部分萨满技能持续时间至指定数值
+    # mod.mod_duration(mod_duration_skills)
+    # 调整 部分萨满技能触发次数至 指定数量
+    # mod.mod_trigger_time(mod_trigger_time_skills)
+
+    # mod spell : 双手斧和锤
+    # 使你可以使用双手斧和双手锤 -> 使你可以使用双手斧,双手锤和双手剑
+    # mod_spell_16269(instance)
 
 
     # 调整 部分萨满技能gcd调整1000ms -> 250ms
