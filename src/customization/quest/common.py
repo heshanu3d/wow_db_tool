454807bab6431d5f6abf2698877ac27530167dff
He Shan <shan.he@nio.com>
2026-04-02 19:13:55 +0800

    complete quest mod

--- src/customization/quest/common.py
+++ src/customization/quest/common.py
@@ -1,8 +1,12 @@
 def mod_quest_request_item_count(instance):
 
-    # instance.fast_select(f'''select title, RequiredItemCount1 cnt1, RequiredItemCount2 cnt2, RequiredItemCount3 cnt3, RequiredItemCount4 cnt4, RequiredItemCount5 cnt5, RequiredItemCount6 cnt6
+    # instance.fast_select(f'''select title, RequiredItemCount1 cnt1, RequiredItemCount2 cnt2, RequiredItemCount3 cnt3, RequiredItemCount4 cnt4, RequiredItemCount5 cnt5, RequiredItemCount6 cnt6, RequiredNpcOrGoCount1 npc_cnt1, RequiredNpcOrGoCount2 npc_cnt2, RequiredNpcOrGoCount3 npc_cnt3, RequiredNpcOrGoCount4 npc_cnt4
     #     from quest_template 
-    #     where title like "%完美宝石%" or title like "%又是铲齿鹿汤%" or title like "%非标准炮弹%";
+    #     where title like "%完美宝石%" or title like "%又是铲齿鹿汤%" or title like "%非标准炮弹%" or title like "%金牙%" or title like "%悬赏：黑石氏族%";
+    # ''')
+    # instance.fast_select(f'''select *
+    #     from quest_template 
+    #     where title like "%非标准炮弹%";
     # ''')
     sql = ''' UPDATE quest_template
                 SET 
@@ -11,7 +15,11 @@ def mod_quest_request_item_count(instance):
                     RequiredItemCount3 = LEAST(RequiredItemCount3, 5),
                     RequiredItemCount4 = LEAST(RequiredItemCount4, 5),
                     RequiredItemCount5 = LEAST(RequiredItemCount5, 5),
-                    RequiredItemCount6 = LEAST(RequiredItemCount6, 5);
+                    RequiredItemCount6 = LEAST(RequiredItemCount6, 5),
+                    RequiredNpcOrGoCount1 = LEAST(RequiredNpcOrGoCount1, 5),
+                    RequiredNpcOrGoCount2 = LEAST(RequiredNpcOrGoCount2, 5),
+                    RequiredNpcOrGoCount3 = LEAST(RequiredNpcOrGoCount3, 5),
+                    RequiredNpcOrGoCount4 = LEAST(RequiredNpcOrGoCount4, 5);
     '''
     instance.execute_multi_sqls(sql)
 
@@ -21,6 +29,3 @@ def customize(instance):
     # 将任务所需的物品数量限制在5个，避免玩家需要收集过多的物品来完成任务，提升游戏体验。
     mod_quest_request_item_count(instance)
 
-
-
-# quest_template
